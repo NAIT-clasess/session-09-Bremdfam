5513870dc31b242f5a68ffd1e1745df38744c651
Brendan Piper <77114803+Bremdfam@users.noreply.github.com>
2026-02-12 03:00:52 -0700

    pong

---
+++ Pong.cs
@@ -0,0 +1,227 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Lesson08_pong_begin;
+
+public class Pong : Game
+{
+    private const int _WindowWidth = 750, _WindowHeight = 450, _BallWidthAndHeight = 21;
+    private const int _PlayAreaEdgeLineWidth = 12;
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    private Texture2D _backgroundTexture, _ballTexture, _paddleTexture;
+
+    private Rectangle _playAreaBoundingBox
+    {
+        get
+        {
+            return new Rectangle(
+            _PlayAreaEdgeLineWidth,
+            _PlayAreaEdgeLineWidth,
+            _WindowWidth - (_PlayAreaEdgeLineWidth * 2),
+            _WindowHeight - (_PlayAreaEdgeLineWidth * 2)
+        );
+        }
+
+        set
+        {
+
+        }
+    }
+
+    private Vector2 _ballPosition, _ballDirection;
+    private Vector2 _paddlePosition, _paddleDirection, _paddleDimensions;
+    private const float _paddleSpeed = 200;
+    private const float _ballSpeed = 160;
+    //Example of read only Property
+    Rectangle _ballRectangle => new Rectangle((int)_ballPosition.X, (int)_ballPosition.Y, _BallWidthAndHeight, _BallWidthAndHeight);
+    Rectangle _paddleRect => new Rectangle((int)_paddlePosition.X, (int)_paddlePosition.Y, _paddleWidth, _paddleHeight);
+
+
+    #region Second Paddle properties
+    private Vector2 _SecondPaddlePosition, _SecondPaddleDirection, _SecondPaddleDimensions;
+    Rectangle _SecondPaddleRect => new Rectangle((int)_SecondPaddlePosition.X, (int)_SecondPaddlePosition.Y, _paddleWidth, _paddleHeight);
+    #endregion
+
+
+    #region  Properties Example
+    int score;
+    private const int _paddleWidth = 6, _paddleHeight = 55;
+
+    int Score
+    {
+        set
+        {
+            score = value;
+            ScoreHasUpdated();
+        }
+        get
+        {
+            var result = score * score;
+            return result;
+        }
+    }
+    public void ScoreHasUpdated()
+    {
+        score *= 2;
+    }
+
+    #endregion
+    public Pong()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        _graphics.PreferredBackBufferWidth = _WindowWidth;
+        _graphics.PreferredBackBufferHeight = _WindowHeight;
+        _graphics.ApplyChanges();
+
+        _ballPosition.X = 150;
+        _ballPosition.Y = 195;
+
+        _ballDirection.X = -1;
+        _ballDirection.Y = -1;
+        //Score =30; // proterty assignment sample
+
+        _paddlePosition = new Vector2(690, 198);
+        _paddleDirection = Vector2.Zero;
+        _paddleDimensions = new Vector2(_paddleWidth, _paddleHeight);
+
+        _SecondPaddlePosition = new Vector2(60, 198);
+        _SecondPaddleDirection = Vector2.Zero;
+
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        _backgroundTexture = Content.Load<Texture2D>("Court");
+        _ballTexture = Content.Load<Texture2D>("Ball");
+        _paddleTexture = Content.Load<Texture2D>("Paddle");
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        #region Comented codes
+        // _ballPosition += _ballDirection * _ballSpeed * (float) gameTime.ElapsedGameTime.TotalSeconds;
+
+        // #region Collision Detection Region
+        //         // bounce ball off left and right sides
+        //         if((_ballPosition.X- _BallWidthAndHeight*0.5f) <= _playAreaBoundingBox.Left ||
+        //            (_ballPosition.X + _BallWidthAndHeight*0.5f) >= _playAreaBoundingBox.Right)
+        //         {
+        //             _ballDirection.X *= -1;
+        //         }
+
+        //         // bounce ball off top and bottom
+        //         if((_ballPosition.Y- _BallWidthAndHeight*0.5f) <= _playAreaBoundingBox.Top ||
+        //            (_ballPosition.Y + _BallWidthAndHeight*0.5f) >= _playAreaBoundingBox.Bottom)
+        //         {
+        //             _ballDirection.Y *= -1;
+        //         }
+        // #endregion
+
+        //          _ballRectangle= new Rectangle((int) _ballPosition.X, (int) _ballPosition.Y, _BallWidthAndHeight, _BallWidthAndHeight);
+        #endregion
+
+
+        #region CollisinDetection
+        if (_ballRectangle.Left <= _playAreaBoundingBox.Left ||
+           _ballRectangle.Right >= _playAreaBoundingBox.Right)
+        {
+            _ballDirection.X *= -1;
+        }
+
+        else if (_ballRectangle.Bottom >= _playAreaBoundingBox.Bottom ||
+          _ballRectangle.Top <= _playAreaBoundingBox.Top)
+        {
+            _ballDirection.Y *= -1;
+        }
+        #endregion
+
+
+        #region  Paddle Controll
+
+        float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        KeyboardState kbState = Keyboard.GetState();
+
+        if (kbState.IsKeyDown(Keys.Up))
+        {
+            _paddleDirection = new Vector2(0, -1);
+        }
+        else if (kbState.IsKeyDown(Keys.Down))
+        {
+            _paddleDirection = new Vector2(0, 1);
+        }
+        else
+        {
+            _paddleDirection = Vector2.Zero;
+        }
+
+        _paddlePosition += _paddleDirection * _paddleSpeed * dt;
+
+        if (_paddlePosition.Y <= _playAreaBoundingBox.Top)
+        {
+            _paddlePosition.Y = _playAreaBoundingBox.Top;
+        }
+        else if (_paddlePosition.Y + _paddleDimensions.Y >= _playAreaBoundingBox.Bottom)
+        {
+            _paddlePosition.Y = _playAreaBoundingBox.Bottom - _paddleDimensions.Y;
+        }
+
+
+
+
+        #endregion
+
+        if (kbState.IsKeyDown(Keys.W))
+        {
+            _SecondPaddleDirection = new Vector2(0, -1);
+        }
+        else if (kbState.IsKeyDown(Keys.S))
+        {
+            _SecondPaddleDirection = new Vector2(0, 1);
+        }
+        else
+        {
+            _SecondPaddleDirection = Vector2.Zero;
+        }
+
+        _SecondPaddlePosition += _SecondPaddleDirection * _paddleSpeed * dt;
+
+        // --- Clamping for Left Paddle ---
+        if (_SecondPaddlePosition.Y <= _playAreaBoundingBox.Top)
+        {
+            _SecondPaddlePosition.Y = _playAreaBoundingBox.Top;
+        }
+        else if (_SecondPaddlePosition.Y + _SecondPaddleDimensions.Y >= _playAreaBoundingBox.Bottom)
+        {
+            _SecondPaddlePosition.Y = _playAreaBoundingBox.Bottom - _SecondPaddleDimensions.Y;
+        }
+        _ballPosition += _ballDirection * _ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+        _spriteBatch.Begin();
+
+        _spriteBatch.Draw(_backgroundTexture, new Rectangle(0, 0, _WindowWidth, _WindowHeight), Color.White);
+        _spriteBatch.Draw(_ballTexture, _ballRectangle, Color.White);
+
+        // Draw both paddles
+        _spriteBatch.Draw(_paddleTexture, _paddleRect, Color.White); // Right
+        _spriteBatch.Draw(_paddleTexture, _SecondPaddleRect, Color.White); // Left
+
+        _spriteBatch.End();
+        base.Draw(gameTime);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿using var game = new MonoTemplate.Game1();
+﻿// using var game = new MonoTemplate.Game1();
+using var game = new Lesson08_pong_begin.Pong();
 game.Run();
